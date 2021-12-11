394044938618a963b717eea4bb32198215ee81e1
David Gurule <davidgurulejr@gmail.com>
2021-12-11 16:45:11 -0700

    trying to figure out three way toggle

--- client/src/components/layout/Navigation.jsx
+++ client/src/components/layout/Navigation.jsx
@@ -5,47 +5,40 @@ import blog from '../../assets/icons/file.png';
 import { useState } from 'react';
 
 const Navigation = () => {
-  const [ isActive, setIsActive] = useState(false);
+  const [ active, setActive] = useState('');
 
-  const list = document.getElementsByClassName("list");
-  const activeLink = () => {
-  // --------------------------------------------------
+ const list = document.getElementsByClassName("list");
+
+  const handleClick = (val) => {
+  for (let i = 0; i < list.length; i++){
+    console.log(list[i]);
+    // list[i].classList.remove("active");
+    // val.classList.add("active");
   }
-  list.forEach((item) => item.addEventListener('click',activeLink));
+  }
   
-  
 
 
 
-
-
-
-
-
-
-
-
-
-  
   return ( 
     <div className="navigation">
       <ul>
 
-        <li className={`list ${isActive ? " active" : ""}`}>
+        <li className="list active" onClick={handleClick}>
           <a href="#">
             <span className="icon"><img id="package" src={product} alt="product icon"/></span>
             <span className="text">PRODUCTS</span> 
           </a>
         </li>
         
-        <li className="list">
+        <li className="list" onClick={handleClick}>
           <a href="#">
             <span className="icon"><img id="bee" src={bee} alt="bee icon"/></span>
             <span className="text">HOME</span> 
           </a>
         </li>
 
-        <li className="list">
+        <li className="list" onClick={handleClick}>
           <a href="#">
             <span className="icon"><img id="blog" src={blog} alt="blog icon"/></span>
             <span className="text">BLOG</span> 
